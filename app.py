ef3df1e81ce88fc71cde8754b3c8da1ac4582336
junyoung <leewnsdud123@gmail.com>
2025-03-08 16:19:54 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_from_directory, request
+import os
+
+app = Flask(__name__)
+
+MEDIA_DIR = "static/media"  # 미디어 파일들이 저장된 폴더
+
+@app.route('/')
+def index():
+    files = os.listdir(MEDIA_DIR)
+    return render_template('index.html', files=files)
+
+@app.route('/media/<filename>')
+def media(filename):
+    return send_from_directory(MEDIA_DIR, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
